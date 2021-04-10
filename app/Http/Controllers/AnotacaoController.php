d414301b4a9d845e2fce61ea0ccd287604762757
Diógenes Castro <diogenesxmarlon@gmail.com>
2021-04-10 18:02:50 -0300

    feat: criados models e controllers iniciais

---
+++ app/Http/Controllers/AnotacaoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Anotacao;
+use Illuminate\Http\Request;
+
+class AnotacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Anotacao  $anotacao
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Anotacao  $anotacao
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Anotacao  $anotacao
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anotacao $anotacao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Anotacao  $anotacao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anotacao $anotacao)
+    {
+        //
+    }
+}
